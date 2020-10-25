873f5aeeec8f81bafd8b3ad052db78265baf0e96
meirmichaeli <36151191+meirmichaeli@users.noreply.github.com>
2020-10-25 18:23:53 +0200

    Update createlocalfile.bat

--- createlocalfile.bat
+++ createlocalfile.bat
@@ -3,12 +3,8 @@ agent any
 stages {
 stage('test') {
 steps {
-dir('C:\\Temp') {
-bat 'echo Meir > Meir4.txt'
-bat 'type Meir4.txt'
-bat 'move Meir4.txt c:\\Temp\\Temp1'
-bat 'fsutil volume diskfree c:'
-}
+dir('C:\\Python') {
+bat 'compare-two-websites.py'
 }
 }
 }
